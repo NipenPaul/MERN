e38a6562d3136118da3cd6093d9f65ad4b365eb0
Nipen Paul <53397507+NipenPaul@users.noreply.github.com>
2023-09-06 00:48:53 +0600

    Create index.js

---
+++ NodeJs/module/index.js
@@ -0,0 +1,15 @@
+// console.log("Hello");
+// setTimeout(() =>{
+//     console.log('test');
+// }, 2000);
+
+//console.log(global);
+
+// console.log(__dirname);
+// console.log(__filename);
+
+const people = require('./people.js');
+
+console.log(people.people);
+console.log(people.a);
+people.test();
