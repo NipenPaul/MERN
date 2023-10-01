59afadb17d8d5efd8a294758c86694007a930742
NipenPaul <nipenpaultpi27@gmail.com>
2023-10-01 13:02:06 +0600

    update

--- grocery-app/app.js
+++ grocery-app/app.js
@@ -37,7 +37,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/api/products', productsRouter); // Use the products router
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
