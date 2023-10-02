4758586291f74354502a457f3049af9f6dcc91fc
NipenPaul <nipenpaultpi27@gmail.com>
2023-10-02 11:26:37 +0600

    leetCode daily task Oct 2, 2023

---
+++ TypeScript/leetCode/remove-colored-pieces-if-both-neighbors-are-the-same-color.ts
@@ -0,0 +1,14 @@
+function winnerOfGame(colors: string): boolean {
+    const len = colors.length;
+    let a = 0, b = 0;
+    for(let i = 1; i < len-1; i++){
+        if(colors[i-1] == 'A' && colors[i] == 'A' && colors[i+1] == 'A'){
+            a++;
+        }
+        else if(colors[i-1] == 'B' && colors[i] == 'B' && colors[i+1] == 'B'){
+            b++;
+        }
+    }
+    if(a > b) return true;
+    return false;
+};
